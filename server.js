4789b6f32f8a0c855206d5455a2729a72b36f903
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-09-13 00:57:34 -0400

    added package.json

--- server.js
+++ server.js
@@ -8,15 +8,13 @@ var htmlRoutes = require("./app/routing/htmlRoutes")
 var app = express();
 
 var PORT = process.env.PORT || 8080;
-//fill in the port with the heroku version
-
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "app/public/home.html");
-
-});
 
 
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
 });
+
+module.exports = {
+
+}
