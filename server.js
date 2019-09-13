e00d9cc88ba96114672aaf072499032f026da442
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-09-12 23:58:59 -0400

    added external dependencies as variables

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-var express=require("express");
-var path=require("path");
+var express = require("express");
+var path = require("path");
+var friends = require("./app/data/friends");
+var apiRoutes = require("./app/routing/apiRoutes");
+var htmlRoutes = require("./app/routing/htmlRoutes")
+
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 //fill in the port with the heroku version
 
-app.get("/", function(req, res) {
-res.sendFile(path.join(__dirname, "app/public/home.html");
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html");
 
 });
 
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
-  });
+});
