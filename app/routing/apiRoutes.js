865c1f33faf6d00c9998caba09ccae44e5d845c4
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-09-14 17:06:35 -0400

    added module exports directories in routes files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,7 @@ var apiFriends = []
 module.exports = {
     getSurvey: app.get("/api/friends", function (req, res) {
         res.JSON(apiFriends)
-    }),
+    }), 
     pushSurvey: app.post("/api/friends", function (req, res) {
 
         console.log(req.body);
